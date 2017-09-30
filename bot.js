6d629683eb908cf4a4a5d993642e7a6fec8fd954
Omar <omar2535@gmail.com>
2017-09-29 23:33:16 -0700

    stuff

--- bot.js
+++ bot.js
@@ -1,10 +1,12 @@
 console.log("hello world");
 
+var Twit = require('twit');
+
 var T = new Twit({
-    consumer_key:         '...',
-    consumer_secret:      '...',
-    access_token:         '...',
-    access_token_secret:  '...',
+    consumer_key:         '	lTtVRLLSQWza2za5tIWbPGgF1',
+    consumer_secret:      '	gvJGlhzgx56wBanOeL5qzU8PlFULkq40KSDanqH1xJ1yxsvDLY',
+    access_token:         '	914011476544724992-zp3OW6QxTLxHSMxwE1Xv9Hrg26YPIqZ',
+    access_token_secret:  '	LhkXkfh3wucghXb4KHAQskfyXaFZsE65w0lZjjg1Vl2lA',
     timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
   });
   
