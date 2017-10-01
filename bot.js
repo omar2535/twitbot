cde125a8d261735226f3a286cb6a9c89b4927942
Omar <omar2535@gmail.com>
2017-09-30 20:38:45 -0700

    make it better

--- bot.js
+++ bot.js
@@ -1,12 +1,56 @@
-console.log("hello world");
 
+//Runs when bot is started up
+console.log("Twitter bot initialized");
+
+//Initializes my variables
 var Twit = require('twit');
+var Config = require('./config');
+var T = new Twit(Config);
+var stream = T.stream('user');
 
-var T = new Twit({
-    consumer_key:         '	lTtVRLLSQWza2za5tIWbPGgF1',
-    consumer_secret:      '	gvJGlhzgx56wBanOeL5qzU8PlFULkq40KSDanqH1xJ1yxsvDLY',
-    access_token:         '	914011476544724992-zp3OW6QxTLxHSMxwE1Xv9Hrg26YPIqZ',
-    access_token_secret:  '	LhkXkfh3wucghXb4KHAQskfyXaFZsE65w0lZjjg1Vl2lA',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
+
+
+
+
+stream.on('follow', function(data){
+  var screenname = data.source.screen_name;
+  console.log(screenname + "followed you");
+  postit(screenname);
+});
+
+stream.on('unfollow', function(data){
+  console.log("someone unfollowed");
+  console.log(data);
+});
+
+stream.on('connected', function (response) {
+  console.log("connected to twtter");
+});
+
+
+
+
+
+
+
+
+//this is to post the tweet saying status
+function postit(name){
+  var random = Math.floor(Math.random()*100);
+  var nametopost = name;
+
+  var topost = {
+    status: "@" + nametopost + " thanks for following me!" + " This is your random number from 0-100: " +  random
+  };
+
+  T.post('statuses/update', topost, function(err, data, response) {
+    if(err){
+      console.log("tweet send failed");
+    }else{
+      console.log("tweet sent successfully");
+      console.log("tweet was" + topost.status);
+    }
   });
-  
+}
+
+
---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    consumer_key:         'lTtVRLLSQWza2za5tIWbPGgF1',
+    consumer_secret:      'gvJGlhzgx56wBanOeL5qzU8PlFULkq40KSDanqH1xJ1yxsvDLY',
+    access_token:         '914011476544724992-zp3OW6QxTLxHSMxwE1Xv9Hrg26YPIqZ',
+    access_token_secret:  'LhkXkfh3wucghXb4KHAQskfyXaFZsE65w0lZjjg1Vl2lA',
+    timeout_ms:           60*1000,
+};
+
